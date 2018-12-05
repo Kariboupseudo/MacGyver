996183be90f1e0f1dc157d6010d32f9426bb4544
Karibou <lebihan@protonmail.com>
2018-12-05 13:42:57 +0100

    optimisation Pep8 grâce à pycodestyle

--- Pygame/classes.py
+++ Pygame/classes.py
@@ -4,15 +4,16 @@ from constants import*
 
 
 class Niveau:
-    """Niveau: This class build an object containing a maze in which it's possible to
-    select a random position and then validate it in order to place items on a proper position."""
+    """Niveau: This class build an object containing a maze
+    in which it's possible to select a random position and then
+    validate it in order to place items on a proper position."""
 
     def __init__(self, level):
         self.lvl = level
         self.structure = self.load_maze()
         self.items = []
         self.random_pos_items()
-     
+
     def random_position(self):
         '''select a random position in the maze where it is possible to move'''
         open_position = []
@@ -37,29 +38,32 @@ class Niveau:
                     self.items = []
                 else:
                     self.items.append(Item(i, candidate_pos))
-        for i in self.items:#slicing objets in self.structure
-            self.structure[i.poscase_y] = self.structure[i.poscase_y][0:i.poscase_x] \
-            + i.display + self.structure[i.poscase_y][i.poscase_x+1:]
-    
+        for i in self.items:  # slicing objets in self.structure
+            self.structure[i.poscase_y] = self.structure[i.poscase_y]\
+                [0:i.poscase_x] + i.display + self.structure[i.poscase_y]\
+                [i.poscase_x+1:]
+
     def valid_pos(self, candidate_pos):
-        """Check if the position of the item is not already taken by a previously created item"""
+        """Check if the position of the item is not
+        already taken by a previously created item"""
         if not self.items:
             answer = True
         else:
             answer = True
             for i, item in enumerate(self.items):
-                if [candidate_pos[0], candidate_pos[1]] == [item.poscase_x, item.poscase_y]:
+                if [candidate_pos[0], candidate_pos[1]] ==\
+                        [item.poscase_x, item.poscase_y]:
                     answer = False
                 elif [candidate_pos[0], candidate_pos[1]] == [2, 2]:
                     answer = False
         return answer
-    
+
     def load_maze(self):
         """Loading a maze structure from a txt file"""
 
         with open(self.lvl + ".txt", "r") as contenu:
             data = contenu.readlines()
-        #suppressing invisible characters
+        # suppressing invisible characters
         for i in range(len(data)):
             data[i] = data[i].strip()
         return data
@@ -67,21 +71,21 @@ class Niveau:
     def display_maze(self, window):
         """Displaying a maze structure associating letters with images"""
 
-        #Wall element
+        # Wall element
         wall = pygame.image.load("images/mur.png").convert()
-        #start element
+        # start element
         start = pygame.image.load("images/depart.png").convert()
-        #Goal element
+        # Goal element
         goal = pygame.image.load("images/Gardien.png").convert()
-        #Item A
+        # Item A
         needle = pygame.image.load("images/needle.png").convert()
-        #Item B
+        # Item B
         syringe = pygame.image.load("images/syringe.png").convert()
-        #Item C
+        # Item C
         pipe = pygame.image.load("images/pipe.png").convert()
-        #Decoration element
+        # Decoration element
         swiss = pygame.image.load("images/swiss.jpg").convert()
-        #Phoenix Foundation Logo
+        # Phoenix Foundation Logo
         phoenix = pygame.image.load("images/phoenix.png").convert()
 
         """double loop to check every character of the txt file"""
@@ -96,7 +100,7 @@ class Niveau:
                 if sprite == "D":
                     window.blit(start, (pos_x, pos_y))
                 if sprite == "G":
-                    window.blit(goal, (pos_x, pos_y)) 
+                    window.blit(goal, (pos_x, pos_y))
                 if sprite == "A":
                     for item in self.items:
                         if item.show is True and item.display == "A":
@@ -132,9 +136,12 @@ class Niveau:
                 num_column += 1
             num_line += 1
 
+
 class Perso:
-    """Perso : builds a character with positionning values both in cases and in pixels.
-    This class needs the level structure and the item list to verify each character's move."""
+    """Perso : builds a character with positionning
+    values both in cases and in pixels.
+    This class needs the level structure and the item
+    list to verify each character's move."""
     def __init__(self, level, items):
         self.poscase_x = 2
         self.poscase_y = 2
@@ -148,76 +155,86 @@ class Perso:
         self.conclusion = None
 
     def move(self, way):
-        """Take a move instruction and execute the move with 
+        """Take a move instruction and execute the move with
         verification of his consequence on the game"""
         if way == "right":
             if self.poscase_x < (nb_sprite_cote - 2):
-                #Checking if landing position is already occupied by an item
+                # Checking if landing position is already occupied by an item
                 for i in self.items:
                     if self.level.structure[self.poscase_y][self.poscase_x+1] \
-                    == i.display and i.show is True:
-                        i.show = False 
-                        self.ens2.add(i.display) #Item is added in Character's inventory
+                            == i.display and i.show is True:
+                        i.show = False
+                        self.ens2.add(i.display)
+                        # Item is added in Character's inventory
                         self.poscase_x += 1
-                        #Calculation of the character's new position in pixels
+                        # Calculation of the character's new position in pixels
                         self.pospix_x = self.poscase_x * taille_sprite
-                #Checking if landing position is the guardian/exit position 
-                if self.level.structure[self.poscase_y][self.poscase_x+1] == 'G':
-                    self.end() #Checking if character's has 3 items in his inventory                     
-                #Checking if landing position is a wall
-                if self.level.structure[self.poscase_y][self.poscase_x+1] != 'W':
+                # Checking if landing position is the guardian/exit position
+                if self.level.structure[self.poscase_y]\
+                        [self.poscase_x+1] == 'G':
+                    self.end()
+                    # Checking if character's has 3 items in his inventory
+                # Checking if landing position is a wall
+                if self.level.structure[self.poscase_y]\
+                        [self.poscase_x+1] != 'W':
                     self.poscase_x += 1
-                    self.pospix_x = self.poscase_x * taille_sprite                  
+                    self.pospix_x = self.poscase_x * taille_sprite
 
-                                  
         if way == "left":
             if self.poscase_x > 0:
                 for i in self.items:
-                    if self.level.structure[self.poscase_y][self.poscase_x-1] == i.display and i.show is True:
+                    if self.level.structure[self.poscase_y][self.poscase_x-1] \
+                            == i.display and i.show is True:
                         i.show = False
                         self.ens2.add(i.display)
                         self.poscase_x -= 1
                         self.pospix_x = self.poscase_x * taille_sprite
-                if self.level.structure[self.poscase_y][self.poscase_x-1] == 'G':
+                if self.level.structure[self.poscase_y]\
+                        [self.poscase_x-1] == 'G':
                     self.end()
-                if self.level.structure[self.poscase_y][self.poscase_x-1] != 'W':
+                if self.level.structure[self.poscase_y]\
+                        [self.poscase_x-1] != 'W':
                     self.poscase_x -= 1
                     self.pospix_x = self.poscase_x * taille_sprite
-         
+
         if way == "up":
             if self.poscase_y > 0:
                 for i in self.items:
-                    if self.level.structure[self.poscase_y-1][self.poscase_x] == i.display and i.show is True:
+                    if self.level.structure[self.poscase_y-1][self.poscase_x]\
+                            == i.display and i.show is True:
                         i.show = False
                         self.ens2.add(i.display)
                         self.poscase_y -= 1
                         self.pospix_y = self.poscase_y * taille_sprite
-                if self.level.structure[self.poscase_y-1][self.poscase_x] == 'G':
+                if self.level.structure[self.poscase_y-1]\
+                        [self.poscase_x] == 'G':
                     self.end()
-                if self.level.structure[self.poscase_y-1][self.poscase_x] != 'W':
+                if self.level.structure[self.poscase_y-1]\
+                        [self.poscase_x] != 'W':
                     self.poscase_y -= 1
                     self.pospix_y = self.poscase_y * taille_sprite
- 
-            
+
         if way == "down":
             if self.poscase_y < (nb_sprite_cote - 2):
                 for i in self.items:
-                    if self.level.structure[self.poscase_y+1][self.poscase_x] ==\
-                    i.display and i.show is True:
+                    if self.level.structure[self.poscase_y+1][self.poscase_x]\
+                            == i.display and i.show is True:
                         i.show = False
                         self.ens2.add(i.display)
                         self.poscase_y += 1
                         self.pospix_y = self.poscase_y * taille_sprite
-                if self.level.structure[self.poscase_y+1][self.poscase_x] == 'G':
-                    self.end()       
-                if self.level.structure[self.poscase_y+1][self.poscase_x] != 'W':
+                if self.level.structure[self.poscase_y+1]\
+                        [self.poscase_x] == 'G':
+                    self.end()
+                if self.level.structure[self.poscase_y+1]\
+                        [self.poscase_x] != 'W':
                     self.poscase_y += 1
                     self.pospix_y = self.poscase_y * taille_sprite
 
     def end(self):
-        '''checking if the victory conditions are met by the character 
+        '''checking if the victory conditions are met by the character
         and return of a contextual image'''
-    
+
         """victory screen"""
         victory = pygame.image.load("images/victory.jpg").convert()
         """lose screen"""
@@ -229,6 +246,7 @@ class Perso:
             self.conclusion = False
             temp = lose
         return temp
+
 
 class Item:
     '''build an item object with positionning values both in cases and pixels
--- Pygame/mac_gyver_maze.py
+++ Pygame/mac_gyver_maze.py
@@ -1,19 +1,23 @@
 """
 Game "Help MacGyver to escape !"
-It's a game where you need to move MacGyver in a maze in order to get out of it.
-MacGyver needs to collect 3 items to be able to beat the guardian.
+It's a game where you need to move MacGyver
+in a maze in order to get out of it.
+MacGyver needs to collect 3 items to be able
+to beat the guardian.
 Python scripts
-Files : mac_gyver_maze.py, classes.py, constants.py, maze.txt + 13 images
+Files : mac_gyver_maze.py, classes.py, constants.py,
+maze.txt + 13 images
 """
 import pygame
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, K_RETURN,\
-                            K_LEFT, K_RIGHT, K_UP, K_DOWN #import pygame constants
+                            K_LEFT, K_RIGHT, K_UP, K_DOWN
+# import pygame constants
 from constants import side_window
 from classes import Niveau, Perso
 
 pygame.init()
 
-#opening a pygame window
+# opening a pygame window
 window = pygame.display.set_mode((side_window, side_window))
 
 """Loading home screen image"""
@@ -25,45 +29,48 @@ background = pygame.image.load("images/background.jpg").convert()
 Main = 1
 while Main:
 
-    window.blit(home, (0, 0))#displaying home screen
-    pygame.display.flip() #window refreshing
+    window.blit(home, (0, 0))  # displaying home screen
+    pygame.display.flip()  # window refreshing
     Menu = 1
     Gaming = 0
     Ending = 0
     """Home screen loop"""
     while Menu:
 
-        start = 0 #variable to start the game
-        pygame.time.Clock().tick(30)#speed limitation of the loop at 30fps
-        for event in pygame.event.get(): #capture events
-            if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+        start = 0  # variable to start the game
+        pygame.time.Clock().tick(30)  # speed limitation of the loop at 30fps
+        for event in pygame.event.get():
+            # capture events
+            if event.type == QUIT or event.type == \
+                    KEYDOWN and event.key == K_ESCAPE:
                 Main = 0
                 Menu = 0
-                Gaming = 0 #Program is shut off
+                Gaming = 0  # Program is shut off
             if event.type == KEYDOWN and event.key == K_RETURN:
-                Menu = 0 #Home screen is shut off
+                Menu = 0  # Home screen is shut off
                 start = "maze"
                 Gaming = 1
 
         if start != 0:
-            #maze generation from a txt file
+            # maze generation from a txt file
             level = Niveau(start)
             level.load_maze()
             level.display_maze(window)
 
-            #Main character creation
+            # Main character creation
             hero = Perso(level, level.items)
 
         while Gaming:
-            #The game is played by moving the main character in a maze.
-            #Main character is controlled with arrow keys."""
+            # The game is played by moving the main character in a maze.
+            # Main character is controlled with arrow keys."""
 
             pygame.time.Clock().tick(30)
             for event in pygame.event.get():
-                if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                if event.type == QUIT or event.type == \
+                        KEYDOWN and event.key == K_ESCAPE:
                     Main = 0
                     Menu = 0
-                    Gaming = 0          
+                    Gaming = 0
                 elif event.type == KEYDOWN:
                     if event.key == K_RIGHT:
                         hero.move('right')
@@ -76,13 +83,13 @@ while Main:
             if Gaming == 0:
                 break
 
-            #Displaying new positions of the main character
+            # Displaying new positions of the main character
             window.blit(background, (0, 0))
             level.display_maze(window)
             window.blit(hero.MG_Display, (hero.pospix_x, hero.pospix_y))
             pygame.display.flip()
 
-            #End of game -> displaying a contextual screen
+            # End of game -> displaying a contextual screen
             if hero.conclusion is None:
                 Main = 0
                 Gaming = 1
@@ -103,15 +110,16 @@ while Main:
                     window.blit(hero.end(), (30, 30))
                     pygame.display.flip()
                 for event in pygame.event.get():
-                    if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                    if event.type == QUIT or event.type == \
+                            KEYDOWN and event.key == K_ESCAPE:
                         Main = 0
                         Menu = 0
                         Gaming = 0
                         Ending = 0
                     elif event.type == KEYDOWN and event.key == K_RETURN:
-                            #Game over screen
-                        thanks = pygame.image.load("images/thanks.jpg").convert()
+                            # Game over screen
+                        thanks = pygame.image.load\
+                            ("images/thanks.jpg").convert()
                         Ending = 0
                         window.blit(thanks, (30, 30))
                         pygame.display.flip()
-                        
